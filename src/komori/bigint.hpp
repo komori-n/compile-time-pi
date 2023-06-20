ec97df6872fac30c8747a51be821bfd84618f251
komori-n <komori-n@ymail.ne.jp>
2023-06-20 22:10:22 +0900

    feat: add `BigInt`

---
+++ src/komori/bigint.hpp
@@ -0,0 +1,121 @@
+#ifndef KOMORI_BIGINT_HPP_
+#define KOMORI_BIGINT_HPP_
+
+#include "komori/biguint.hpp"
+
+namespace komori {
+class BigInt {
+ public:
+  constexpr BigInt(uint64_t value, Sign sign = Sign::kPositive) : value_{value}, sign_{sign} {}
+  constexpr BigInt(std::vector<uint64_t> values, Sign sign = Sign::kPositive)
+      : value_{std::move(values)}, sign_{sign} {}
+  constexpr BigInt(std::initializer_list<uint64_t> values, Sign sign = Sign::kPositive)
+      : value_{std::move(values)}, sign_{sign} {}
+  constexpr BigInt(BigUint value, Sign sign) : value_{std::move(value)}, sign_{sign} {}
+
+  constexpr BigInt() = default;
+  constexpr BigInt(const BigInt&) = default;
+  constexpr BigInt(BigInt&&) noexcept = default;
+  constexpr BigInt& operator=(const BigInt&) = default;
+  constexpr BigInt& operator=(BigInt&&) noexcept = default;
+  constexpr ~BigInt() = default;
+
+  constexpr bool IsZero() const noexcept { return value_.IsZero(); }
+
+  constexpr BigInt& operator+=(const BigInt& rhs) {
+    if (sign_ == rhs.sign_) {
+      value_ += rhs.value_;
+    } else if (value_ >= rhs.value_) {
+      value_ -= rhs.value_;
+    } else {
+      value_ = rhs.value_ - std::move(value_);
+      sign_ = rhs.sign_;
+    }
+
+    return *this;
+  }
+
+  constexpr BigInt& operator-=(const BigInt& rhs) {
+    if (sign_ != rhs.sign_) {
+      value_ += rhs.value_;
+    } else if (value_ >= rhs.value_) {
+      value_ -= rhs.value_;
+    } else {
+      value_ = rhs.value_ - std::move(value_);
+      sign_ = ~sign_;
+    }
+
+    return *this;
+  }
+
+  constexpr BigInt& operator*=(const BigInt& rhs) {
+    value_ *= rhs.value_;
+    sign_ = sign_ ^ rhs.sign_;
+    return *this;
+  }
+
+  constexpr BigInt& operator>>=(const std::size_t rhs) {
+    value_ >>= rhs;
+    return *this;
+  }
+
+  constexpr BigInt& operator<<=(const std::size_t rhs) {
+    value_ <<= rhs;
+    return *this;
+  }
+
+  friend constexpr BigInt operator+(const BigInt& lhs, const BigInt& rhs) {
+    BigInt tmp = lhs;
+    tmp += rhs;
+    return tmp;
+  }
+
+  friend constexpr BigInt operator-(const BigInt& lhs, const BigInt& rhs) {
+    BigInt tmp = lhs;
+    tmp -= rhs;
+    return tmp;
+  }
+
+  friend constexpr BigInt operator-(const BigInt& rhs) { return {rhs.value_, ~rhs.sign_}; }
+
+  friend constexpr BigInt operator*(const BigInt& lhs, const BigInt& rhs) {
+    BigInt tmp = lhs;
+    tmp *= rhs;
+    return tmp;
+  }
+
+  friend constexpr BigInt operator>>(const BigInt& lhs, const std::size_t& rhs) {
+    return {lhs.value_ >> rhs, lhs.sign_};
+  }
+  friend constexpr BigInt operator<<(const BigInt& lhs, const std::size_t& rhs) {
+    return {lhs.value_ << rhs, lhs.sign_};
+  }
+  friend constexpr std::strong_ordering operator<=>(const BigInt& lhs, const BigInt& rhs) {
+    if (lhs.sign_ == Sign::kPositive && rhs.sign_ == Sign::kPositive) {
+      return lhs.value_ <=> rhs.value_;
+    } else if (lhs.sign_ == Sign::kNegative && rhs.sign_ == Sign::kNegative) {
+      const auto ordering = lhs.value_ <=> rhs.value_;
+      if (ordering == std::strong_ordering::greater) {
+        return std::strong_ordering::less;
+      } else if (ordering == std::strong_ordering::less) {
+        return std::strong_ordering::greater;
+      } else {
+        return std::strong_ordering::equal;
+      }
+    } else if (lhs.sign_ == Sign::kPositive && rhs.sign_ == Sign::kNegative) {
+      return std::strong_ordering::greater;
+    } else {
+      // lhs.sign_ == Sign::kNegative && rhs.sign_ == Sign::kPoisitive
+      return std::strong_ordering::less;
+    }
+  }
+
+  friend constexpr bool operator==(const BigInt& lhs, const BigInt& rhs) = default;
+
+ private:
+  BigUint value_{};
+  Sign sign_{Sign::kPositive};
+};
+}  // namespace komori
+
+#endif  // KOMORI_BIGINT_HPP_
---
+++ src/tests/bigint_test.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+
+#include "komori/bigint.hpp"
+
+using komori::BigInt;
+using komori::Sign;
+
+TEST(BigInt, IsZero) {
+  EXPECT_TRUE(BigInt{}.IsZero());
+  EXPECT_FALSE(BigInt(0x334ULL).IsZero());
+  EXPECT_FALSE(BigInt(0x334ULL, Sign::kNegative).IsZero());
+}
+
+TEST(BigInt, Add) {
+  const BigInt x(0x334ULL);
+  const BigInt y(0x264ULL);
+
+  EXPECT_EQ(x + BigInt{}, x);
+  EXPECT_EQ(y + BigInt{}, y);
+  EXPECT_EQ((-x) + BigInt{}, -x);
+  EXPECT_EQ((-y) + BigInt{}, -y);
+
+  EXPECT_EQ(x + y, BigInt(0x334ULL + 0x264ULL));
+  EXPECT_EQ(x + (-y), BigInt(0x334ULL - 0x264ULL));
+  EXPECT_EQ((-x) + y, BigInt(0x334ULL - 0x264ULL, Sign::kNegative));
+  EXPECT_EQ((-x) + (-y), BigInt(0x334ULL + 0x264ULL, Sign::kNegative));
+
+  EXPECT_EQ(y + x, BigInt(0x334ULL + 0x264ULL));
+  EXPECT_EQ(y + (-x), BigInt(0x334ULL - 0x264ULL, Sign::kNegative));
+  EXPECT_EQ((-y) + x, BigInt(0x334ULL - 0x264ULL));
+  EXPECT_EQ((-y) + (-x), BigInt(0x334ULL + 0x264ULL, Sign::kNegative));
+}
+
+TEST(BigInt, Sub) {
+  const BigInt x(0x334ULL);
+  const BigInt y(0x264ULL);
+
+  EXPECT_EQ(x + BigInt{}, x);
+  EXPECT_EQ(y + BigInt{}, y);
+  EXPECT_EQ((-x) + BigInt{}, -x);
+  EXPECT_EQ((-y) + BigInt{}, -y);
+
+  EXPECT_EQ(x - y, BigInt(0x334ULL - 0x264ULL));
+  EXPECT_EQ(x - (-y), BigInt(0x334ULL + 0x264ULL));
+  EXPECT_EQ((-x) - y, BigInt(0x334ULL + 0x264ULL, Sign::kNegative));
+  EXPECT_EQ((-x) - (-y), BigInt(0x334ULL - 0x264ULL, Sign::kNegative));
+
+  EXPECT_EQ(y - x, BigInt(0x334ULL - 0x264ULL, Sign::kNegative));
+  EXPECT_EQ(y - (-x), BigInt(0x334ULL + 0x264ULL));
+  EXPECT_EQ((-y) - x, BigInt(0x334ULL + 0x264ULL, Sign::kNegative));
+  EXPECT_EQ((-y) - (-x), BigInt(0x334ULL - 0x264ULL));
+}
+
+TEST(BigInt, Mul) {
+  const BigInt x(0x334ULL);
+  const BigInt y(0x264ULL);
+
+  EXPECT_EQ(x * y, BigInt(0x334ULL * 0x264ULL));
+  EXPECT_EQ(x * (-y), BigInt(0x334ULL * 0x264ULL, Sign::kNegative));
+  EXPECT_EQ((-x) * y, BigInt(0x334ULL * 0x264ULL, Sign::kNegative));
+  EXPECT_EQ((-x) * (-y), BigInt(0x334ULL * 0x264ULL));
+}
+
+TEST(BigInt, Shl) {
+  const BigInt x(0x334ULL);
+  const BigInt expected(0x3340ULL);
+
+  EXPECT_EQ(x << 4, expected);
+  EXPECT_EQ((-x) << 4, -expected);
+}
+
+TEST(BigInt, Shr) {
+  const BigInt x(0x334ULL);
+  const BigInt expected(0x33ULL);
+
+  EXPECT_EQ(x >> 4, expected);
+  EXPECT_EQ((-x) >> 4, -expected);
+}
+
+TEST(BigInt, Comparison) {
+  const BigInt x(0x334ULL);
+  const BigInt y(0x264ULL);
+
+  EXPECT_TRUE(x > y);
+  EXPECT_TRUE(x > (-y));
+  EXPECT_TRUE((-x) < y);
+  EXPECT_TRUE((-x) < (-y));
+}
